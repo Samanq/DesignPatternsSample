9655510a3ffa4c4d09ca9af742e79a83d24b38c2
Saman Qaydi (LAP01) <saman.qaydi@gmail.com>
2022-08-24 22:04:51 +0200

    Update comments in SimpleFactory sample

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # DesignPatternsSample
 Sample for implementing design patterns in C#
 
-## Simple Factory
+>## Simple Factory
 
 UML:
 ![Simple Factory UML](./assets/images/umls/simpleFactory.jpg "Simple Factory UML")
 
 ---
-## Factory Method
+>## Factory Method
 
 ---
 
-## Abstract Factory
+>## Abstract Factory
 
 ---
--- src/FactoryPatterns/SimpleFactory/FoodFactory.cs
+++ src/FactoryPatterns/SimpleFactory/FoodFactory.cs
@@ -3,6 +3,7 @@
 public static class FoodFactory
 {
     // Here we broke open closed principle wich is not a good practice.
+    // Because everytime that we need to create a new type of food we have to modify the FoodFactory class and add another case for the new type.
     public static IFood? GetFood(string title)
     {
         title = title.ToLower().Trim();
